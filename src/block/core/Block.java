ab3fc3d540ca0a345645c2e6701f97e0f56cec08
Ian J.Um <31336310+code0xff@users.noreply.github.com>
2018-05-20 23:24:23 +0900

    edit bits

--- src/block/core/Block.java
+++ src/block/core/Block.java
@@ -23,7 +23,7 @@ public class Block {
 		String genesisMerkle = "4a5e1e4baab89f3a32518a88c31bc87f618f76673e2cc77ab2127b7afdeda33b";
 		genesisBlock.setMerkleHash(hexUtil.getBytesFromReverseHex(new byte[32], genesisMerkle));
 		genesisBlock.setTime(1231006505);
-		genesisBlock.setBits(0x1d00ffff);
+		genesisBlock.setBits(0x1f00ffff);
 		genesisBlock.setNonce(2083236893);
 
 		return genesisBlock;
